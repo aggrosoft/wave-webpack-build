9d975c4bc70d8e98dc151c4f0943c5e917d881ee
alexander <coding@aggrosoft.de>
2021-01-28 16:42:53 +0100

    use cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,12 @@ module.exports = {
     // but make sure you install these via yarn or npm!
 
     // add browserslist config to package.json (see below)
-    autoprefixer: {}
+    autoprefixer: {},
+    cssnano: {
+      preset: [
+        "default",
+        {"discardComments": {"removeAll": true}}
+      ]
+    }
   }
 }
