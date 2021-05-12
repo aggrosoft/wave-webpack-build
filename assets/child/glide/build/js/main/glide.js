e76e9470737749e15bbf82d0f94bd8be90d30316
alexander <coding@aggrosoft.de>
2021-05-12 15:56:08 +0200

    allow custom sliders

---
+++ assets/child/glide/build/js/main/glide.js
@@ -0,0 +1,9 @@
+import "@glidejs/glide/src/assets/sass/glide.core.scss";
+import "@glidejs/glide/src/assets/sass/glide.theme.scss";
+import Glide, { Controls, Breakpoints, Images, Swipe, Anchors } from '@glidejs/glide/dist/glide.modular.esm'
+
+const sliders = document.getElementsByClassName('glide-slider')
+
+sliders.forEach(slider => {
+  const glide = new Glide(slider, JSON.parse(slider.dataset.glide)).mount({ Controls, Breakpoints, Images, Swipe, Anchors })
+})
